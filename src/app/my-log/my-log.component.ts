de2f0e5626e9c3fe494747d5b26fe425151045bc
Nikhil T Nair <nikhil.nair@pharmeasy.in>
2017-08-25 12:35:45 +0530

    added filters

--- src/app/my-log/my-log.component.ts
+++ src/app/my-log/my-log.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit} from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
 import { DataService } from '../services/data.service';
-
 import * as _ from 'underscore';
 import * as moment from 'moment';
 
@@ -18,48 +17,77 @@ export class MyLogComponent implements OnInit {
   userData: Dt[];
   showDialog = false;
   filter = {
-    loggedUseremail: '',
-    displayTime: moment(new Date).format('YYYY-MM-DD'),
-    email: '',
-    time: moment(new Date).format('YYYY-MM-DD')
+    email: [],
+    startDate: '',
+    endDate: ''
   };
+  filteredDt = {
+    email: '',
+    startDate: '',
+    endDate: ''
+  }
 
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
-
-    this.getLogData();
-  }
-
-  getLogData() {
     this.dataService.getData()
     .subscribe((res) => {
       this.userData = res;
-      var newArr = _.map(this.userData, function(element) {
-        return _.extend(
-          {}, 
-          element, {
-            date: moment(element['time'], 'DD-MM-YYYY').format('dddd, MMMM DD'),
-            displayTime: moment(element['time'], 'DD-MM-YYYY hh:mm').format('h:mm A')
-          });
-      });
-      this.data = _.groupBy(newArr, 'date');
-      this.keyList = Object.keys(this.data);
-      console.log(this.data);
-      console.log(this.keyList);
-    });
+      var queryParams = this.route.snapshot.queryParams;
+      console.log('queryParams is ', queryParams);
+      _.each(queryParams, (val, key) => {
+        this.filter[key] = this.filteredDt[key] = val;
+      })
+      this.setFilter(queryParams);
+    })
+  };
+
+  // var newArr = _.map(this.userData, function(element) {
+  //   return _.extend(
+  //     {}, 
+  //     element, {
+  //       date: moment(element['time'], 'DD-MM-YYYY').format('dddd, MMMM DD'),
+  //       displayTime: moment(element['time'], 'DD-MM-YYYY hh:mm').format('h:mm A')
+  //     });
+  // });
+  // this.data = _.groupBy(newArr, 'date');
+  // this.keyList = Object.keys(this.data);
+
+  generateFilter(filterDt) {
+    var afterFilter={};
+    for (let key in filterDt) {
+      if (filterDt[key]) {
+        afterFilter[key] = filterDt[key];
+      }
+    }
+    this.router.navigate(['/'],{ queryParams: afterFilter});
+    this.showDialog = false;
+    this.setFilter(afterFilter);
   }
 
   setFilter(filterDt) {
-    this.showDialog = false;
-    var filtered = _.filter(this.userData, function(item){ 
-      return((filterDt.loggedUseremail ? item.loggedUseremail === filterDt.loggedUseremail : true) && (filterDt.displayTime ? moment(item.time).format('YYYY-DD-MM') === filterDt.displayTime : true));
+    console.log(filterDt, this.userData);
+    var filteredInfo = _.filter(this.userData, function(item){
+      var splitDate: string[] = moment(item.time).format('YYYY-MM-DD').split('-'),
+      splitDt = new Date(Number(splitDate[0]), Number(splitDate[1]), Number(splitDate[2])),
+      startDate = filterDt.startDate ? new Date(filterDt.startDate.split('-')[0], filterDt.startDate.split('-')[1], filterDt.startDate.split('-')[2]) : '',
+      endDate = filterDt.endDate ? new Date(filterDt.endDate.split('-')[0],filterDt.endDate.split('-')[1], filterDt.endDate.split('-')[2]) : '';
+      console.log('splitDt is ', splitDt);
+      console.log('start date is', startDate);
+      console.log('end date is ', endDate);
+      return(
+        filterDt.email ? item.loggedUseremail.includes(filterDt.email) : '' &&
+        filterDt.startDate ? startDate <= splitDt: '' &&
+        filterDt.endDate ? endDate >= splitDt: ''
+      );
     });
-    console.log(filtered);
-    if(filtered) {
-      var newArr = _.map(filtered, function(element) {
+    console.log('filteredInfo is ', filteredInfo);
+    if(filteredInfo) {
+      var newArr = _.map(filteredInfo, function(element) {
         return _.extend(
           {}, 
           element, {
@@ -70,37 +98,15 @@ export class MyLogComponent implements OnInit {
       this.data = _.groupBy(newArr, 'date');
     }
     this.keyList = Object.keys(this.data);
-    this.filter.email = filterDt.loggedUseremail;
-    this.filter.time = filterDt.displayTime;
   }
 
   openDialog() {
     this.showDialog = !this.showDialog;
   }
 
-
-daterangepickerOptions = {
-    startDate: '09/01/2017',
-    endDate: '09/02/2017',
-    format: 'DD/MM/YYYY'
+  onMultiple(data: Array<string>): void {
+    this.filter.email = data;
   }
-
-
-    /* multipl select code */
-
-     multipleData = [];
-     selectDisabled = true;
-
-     userModel = {
-         gender: "m",
-         name: "John Doe"
-     };
-     selection: string = "";
-     cities: Array<string> = ["New York", "Belgrade", "Stockholm", "Sarajevo"];
-
-     onMultiple(data: Array<string>): void {
-         this.multipleData = data;
-     }
 }
 
 interface Data {
